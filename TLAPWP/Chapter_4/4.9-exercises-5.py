6be1c8cc2bdae84ccc34248d52042bd8fc9ff8a3
TSTKTCHN <hhe0070@gmail.com>
2023-07-08 10:47:47 -0700

    4.9 q5

---
+++ TLAPWP/Chapter_4/4.9-exercises-5.py
@@ -0,0 +1,26 @@
+# Make the shapes
+# Reference pp. 51 question 5
+
+import turtle
+
+turt = turtle.Turtle()
+turt.hideturtle()
+bg = turtle.Screen()
+bg.bgcolor('lightgreen')
+
+def spiralrotate(functurt, n):
+    functurt = turtle.Turtle()
+    functurt.color('blue')
+    functurt.width(3)
+    functurt.speed(10)
+    x = 0
+    angle = 90
+    for i in range(n):
+        x = x+2.5
+        angle = angle +.005
+        functurt.right(angle)
+        functurt.forward(x)
+
+spiralrotate(turt, 200)
+
+turtle.done()
