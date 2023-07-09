1fc5aac1b0ddef59949c6a6081b283f5a4b4e545
TSTKTCHN <hhe0070@gmail.com>
2023-07-09 08:22:07 -0700

    4.9 q8

---
+++ TLAPWP/Chapter_4/4.9-execises-8.py
@@ -0,0 +1,9 @@
+# Write a function area_of_circle(r) which returns the area of a circle of radius r.
+
+def area_of_circle(r):
+    pi = 3.14
+    radius = r
+    area = pi*(r**2)
+    print(area)
+
+area_of_circle(10)
