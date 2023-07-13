d28d4bfcb73b046dd2f353beaec6bd4b574a79e2
TSTKTCHN <hhe0070@gmail.com>
2023-07-12 17:11:25 -0700

    5.14 question 1

---
+++ TLAPWP/Chapter_5/5.13-1.1.py
@@ -0,0 +1,15 @@
+# 5.14 Question 1
+# 1. Assume the days of the week are numbered 0,1,2,3,4,5,6 from Sunday to Saturday. Write
+# a function which is given the day number, and it returns the day name (a string).
+
+# Call n element in an array
+
+# init array
+days = [0, 1, 2, 3, 4, 5, 6]
+days[0] = "Monday"
+days[1] = "Tuesday"
+days[3] = "Wednesday"
+days[4] = "Thursday"
+days[5] = "Friday"
+
+print(days[1])
