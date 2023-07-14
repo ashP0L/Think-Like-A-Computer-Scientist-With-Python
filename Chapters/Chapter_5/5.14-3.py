b8d6232688f9227160944877a1d0cc974aba7f9c
TSTKTCHN <hhe0070@gmail.com>
2023-07-14 15:29:25 -0700

    5.14-3

---
+++ Chapters/Chapter_5/5.14-3.py
@@ -0,0 +1,11 @@
+# Give the logical opposite of these conditions
+
+#(a) a > b
+#(b) a >= b
+#(c) a >= 18 and day == 3
+#(d) a >= 18 and day != 3
+
+# a <= b
+# a < b
+# a < 18 or day =! 3
+# a < 18 or day == 3
