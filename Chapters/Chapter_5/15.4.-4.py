beeaa8596a6a6b7a6a565d9a66ce877d6a52db70
TSTKTCHN <hhe0070@gmail.com>
2023-07-14 15:42:46 -0700

    5.14-4

---
+++ Chapters/Chapter_5/15.4.-4.py
@@ -0,0 +1,18 @@
+# Evaluate the expressions
+
+# (a) 3 == 3
+# (b) 3 != 3
+# (c) 3 >= 4
+# (d) not (3 < 4)
+
+# (a)
+print("a. ", 3 ==3)
+
+# (b)
+print("b. ", 3 != 3)
+
+#(c)
+print("c. ", 3 >= 4)
+
+#(d)
+print("d. ", not (3 < 4))
