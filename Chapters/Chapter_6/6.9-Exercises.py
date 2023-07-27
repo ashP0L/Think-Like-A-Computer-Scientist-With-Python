934aab62ceefda3580890467b841992275676b05
TSTKTCHN <TSTKTCHN.GIT@gmail.com>
2023-07-27 08:50:30 -0700

    6.9

--- Chapters/Chapter_6/6.9-Exercises.py
+++ Chapters/Chapter_6/6.9-Exercises.py
@@ -35,7 +35,7 @@ test(turn_clockwise("N") == "E")
 test(turn_clockwise("W") == "N")
 test(turn_clockwise("42") == None)
 test(turn_clockwise("rubbish") == None)
-print("Test for turn_clockwise completed. \n\n")
+print("Test for turn_clockwise completed. \n")
 
 # 2
 # Write a function day_name that converts an integer number 0 to 6 into the name of a
@@ -67,7 +67,7 @@ test(weekday(3) == "Wednesday")
 test(weekday("rubbish") == None)
 test(weekday(42) == None)
 
-print("Testing completed for weekday.\n\n")
+print("Testing completed for weekday.\n")
 
 #3 Write the inverse function day_num which is given a day name, and returns its number:
 
@@ -97,9 +97,18 @@ test(day_num(weekday(3)) == 3) # fruit of day_num and weekday.
 test(weekday(day_num("Wednesday")) == "Wednesday")
 test(day_num("Wrong"))
 
-print("\nTesting completed for day_num \n\n")
+print("\nTesting completed for day_num \n")
 
-#4
+# 4.
+# Write a function that helps answer questions like “‘Today is Wednesday. I leave on
+# holiday in 19 days time. What day will that be?”’ So the function must take a day name
+# and a delta argument — the number of days to add — and should return the resulting
+# day name:
+
+# && 5. 
+# Can your day_add function already work with negative deltas? For example, -1 would
+# be yesterday, or -7 would be a week ago:
+
 def day_print(ya):
     if ya == 1:
         return "Monday"
@@ -137,7 +146,7 @@ def day_add(day,x):
         y = (d + x) % 7
     else:
         if x > -7:
-            y = 7 - (7 - (abs(d + x) % 7))
+            y = 7 - (7 - (abs(d + x))) # 7 minus abs operation because we're going backwards from sunday.
         else:
             y = (7 - (abs(d + x) % 7))
     return day_print(y)
@@ -154,7 +163,88 @@ test(day_add("Tuesday", -15) == "Monday")
 test(day_add("Monday", -10) == "Friday")
 test(day_add("Friday", -1) == "Thursday")
 
-print("\nTesting completed for day_add and day_print\n\n")
+print("\nTesting completed for day_add and day_print\n")
 
+# 6.
+# Write a function days_in_month which takes the name of a month, and returns the
+# number of days in the month. Ignore leap years:
 
+def days_in_month(month):
+    if month == "January":
+        return 31
+    elif month == "February":
+        return 28
+    elif month == "March":
+        return 31
+    elif month == "April":
+        return 30
+    elif month == "May":
+        return 31
+    elif month == "June":
+        return 30
+    elif month == "July":
+        return 31
+    elif month == "August":
+        return 31
+    elif month == "September":
+        return 30
+    elif month == "October":
+        return 31
+    elif month == "November":
+        return 30
+    elif month == "December":
+        return 31
+    else:
+        return None
+# test cass
+
+print("\nStarting test for days_in_month\n")
+test(days_in_month("February") == 28)
+test(days_in_month("December") == 31)
+test(days_in_month("Junk") == 31)
+print("\nTesting for days_in_month complete\n")
+
+# 7. Write a function to_secs that converts hours, minutes and seconds to a total number
+# of seconds.
+
+def to_secs(h,m,s):
+    return ((h * 3600) + (m * 60) + (s))
+
+print("\nStarting test for to_secs:\n")
+test(to_secs(2, 30, 10) == 9010)
+test(to_secs(2, 0, 0) == 7200)
+test(to_secs(0, 2, 0) == 120)
+test(to_secs(0, 0, 42) == 42)
+test(to_secs(0, -10, 10) == -590)
+print("\nTesting completed for to_secs\n")
+
+# 8. Extend to_secs so that it can cope with real values as inputs. It should always return
+# an integer number of seconds (truncated towards zero):
+
+def to_secs_2(h,m,s):
+    x = int(((h * 3600) + (m * 60) + (s))) # truncate towards zero with int()
+    return x
+
+print("\nStarting test for test_to_sec_2\n")
+test(to_secs_2(2.5, 0, 10.71) == 9010)
+test(to_secs_2(2.433, 0, 0) == 8758)
+print("\nTesting completed for test_to_sec_2\n")
+
+# 9. Write three functions that are the “inverses” of to_secs:
+# (a) hours_in returns the whole integer number of hours represented by a total num-
+# ber of seconds.
+# (b) minutes_in returns the whole integer number of left over minutes in a total
+# number of seconds, once the hours have been taken out.
+# (c) seconds_in returns the left over seconds represented by a total number of sec-
+# onds.
+
+# 9(a)
+def hours_in(s):
+    return int(s/3600)
+
+def minutes_in
+
+test(hours_in(9010) == 2)
+test(minutes_in(9010) == 30)
+test(seconds_in(9010) == 10)    
 
